ba33a24dff9286e6301bd159fe4f36e297eea85c
JellyGodKyrie <49534828+JellyGodKyrie@users.noreply.github.com>
2020-01-19 19:13:56 -0500

    fix some linting errors and update eslint parser

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Aux from '../../hoc/Auxiliary';
-import Burger from '../../components/Burger/Burger';
-import BuildControls from '../../components/Burger/BuildControls/BuildControls';
-import Modal from '../../components/UI/Modal/Modal';
-import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import Aux from "../../hoc/Auxiliary";
+import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+import Modal from "../../components/UI/Modal/Modal";
+import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 
-
-const INGREDIENT_PRICES ={
+const INGREDIENT_PRICES = {
     salad: 0.7,
     cheese: 0.5,
     bacon: 1.2,
-    meat: 1.6
-}
+    meat: 1.6,
+};
 class BurgerBuilder extends Component {
     // constructor(props) {
     //     super(props);
@@ -24,37 +23,38 @@ class BurgerBuilder extends Component {
             salad: 0,
             bacon: 0,
             cheese: 0,
-            meat: 0
+            meat: 0,
         },
         totalPrice: 5,
         purchasable: false,
-        purchasing: false
-    }
+        purchasing: false,
+    };
 
-    updatePurchaseState (ingredients) {
+    updatePurchaseState(ingredients) {
         const sum = Object.keys(ingredients)
-            .map(igKey => {
+            .map((igKey) => {
                 return ingredients[igKey];
-            } )
-            .reduce( (sum, el) => {
-                return sum + el;
+            })
+            .reduce((acc, el) => {
+                return acc + el;
             }, 0);
-        this.setState( {purchasable: sum > 0} );
+        this.setState({ purchasable: sum > 0 });
     }
 
     addIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
         const updatedCount = oldCount + 1;
         const updatedIngredients = {
-            ...this.state.ingredients
+            // eslint-disable-next-line react/no-access-state-in-setstate
+            ...this.state.ingredients,
         };
         updatedIngredients[type] = updatedCount;
         const priceAddition = INGREDIENT_PRICES[type];
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
-        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
         this.updatePurchaseState(updatedIngredients);
-    }
+    };
 
     removeIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
@@ -63,55 +63,59 @@ class BurgerBuilder extends Component {
         }
         const updatedCount = oldCount - 1;
         const updatedIngredients = {
-            ...this.state.ingredients
+            // eslint-disable-next-line react/no-access-state-in-setstate
+            ...this.state.ingredients,
         };
         updatedIngredients[type] = updatedCount;
         const priceDeduction = INGREDIENT_PRICES[type];
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - priceDeduction;
-        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
         this.updatePurchaseState(updatedIngredients);
-    }
+    };
 
     purchaseHandler = () => {
-        this.setState({purchasing: true});
-    }
+        this.setState({ purchasing: true });
+    };
 
     purchaseCancelHandler = () => {
-        this.setState({purchasing: false});
-    }
+        this.setState({ purchasing: false });
+    };
 
     purchaseContinueHandler = () => {
         alert("You continue!");
-    }
+    };
 
-    render () {
+    render() {
         const disabledInfo = {
-            ...this.state.ingredients
+            ...this.state.ingredients,
         };
-        for (let key in disabledInfo) {
-            disabledInfo[key] = disabledInfo[key] <= 0
+        // eslint-disable-next-line
+        for (const key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
         }
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary 
+                    <OrderSummary
                         ingredients={this.state.ingredients}
                         price={this.state.totalPrice}
                         purchaseCancelled={this.purchaseCancelHandler}
-                        purchaseContinued={this.purchaseContinueHandler} />
+                        purchaseContinued={this.purchaseContinueHandler}
+                    />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
-                <BuildControls 
+                <BuildControls
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
                     purchasable={this.state.purchasable}
                     ordered={this.purchaseHandler}
-                    price={this.state.totalPrice} />
+                    price={this.state.totalPrice}
+                />
             </Aux>
         );
     }
-};
+}
 
 export default BurgerBuilder;
